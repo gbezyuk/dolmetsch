1042ec2669a1c46bd7618f016374f2bbadd66ca6
gbezyuk <dev@gbezyuk.ru>
2021-11-05 15:26:54 +0300

    navigation layout fixes

--- vanilla/scripts/layout.js
+++ vanilla/scripts/layout.js
@@ -1,12 +1,12 @@
 const primaryNavigationHTML = `
-	<a href="/"><h1>Dolmetsch</h1></a>
+	<a href="/"><h2>Dolmetsch</h2></a>
 	<hr>
 	<a href="./index.html">about</a>
 	<a href="https://github.com/gbezyuk/dolmetsch" target="_blank">GitHub</a>
 	<hr>
 	<a href="./reader.html"><strong>Reader</strong></a>
 	<hr>
-	<h2>Dict</h2>
+	<h4>Dict</h4>
 	<a href="./dict-viewer.html">view/edit</a>
 	<a href="./dict-export.html">export</a>
 `
